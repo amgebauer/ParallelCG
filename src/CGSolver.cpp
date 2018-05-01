8cb101a3337f041d91e229460789b755674d858c
Amadeus Gebauer <amadeus.gebauer@gmail.com>
2018-05-01 02:14:49 +0200

    Added travis

--- src/CGSolver.cpp
+++ src/CGSolver.cpp
@@ -20,22 +20,20 @@ void SOLVE::CGSolver::solveSerial(const LINALG::SymmetricMatrix &matrix,
 
     int iter = 0;
     while (r_squared > epsilonSquared) {
-        const LINALG::Vector tmpVector = matrix*p; // compute here part of the matrix product
+        const LINALG::Vector tmpVector = matrix*p;
 
-        double alpha = r_squared/(p*tmpVector); // exchange part of the scalar product with other nodes - EXCHANGE scalar
+        double alpha = r_squared/(p*tmpVector);
 
-        result.add(alpha, p); // compute part of new x
-        r.add(-alpha, tmpVector); // compute part of new r
+        result.add(alpha, p);
+        r.add(-alpha, tmpVector);
 
-        double r_squared_new = r*r; // compute part of scalar product and communicate part of it - EXCHANGE scalar
-        double beta = r_squared_new / r_squared; // compute new beta
-        r_squared = r_squared_new;  // override r_squared
+        double r_squared_new = r*r;
+        double beta = r_squared_new / r_squared;
+        r_squared = r_squared_new;
 
-        p.add(1, r, beta); // compute part of p and exchange with other nodes - EXCHANGE vector
+        p.add(1, r, beta);
         iter += 1;
     }
-
-    // MPI IO for x
 }
 
 void SOLVE::CGSolver::solveParallel(const LINALG::DistributedSymmetricMatrix &matrix,
